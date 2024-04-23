86045e88e452e3d5e7e422564db40a93aa84a6b7
Dai <behoya11@gmail.com>
2024-04-23 14:23:40 +0800

    feat: Build Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div className="w-full bg-slate-100 py-1 text-center text-sm text-slate-600">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+      Coded by <a href="#">Dyy</a>.
+    </div>
+  );
+}
+
+export default Footer;
